0234c328af880ad3386f25ab5dfaf5957e379b25
이승훈 <negrat@naver.com>
2022-02-09 10:46:33 +0900

    이메일 / 비번 파라미터 받아보기

--- copang_app/api/user/user.py
+++ copang_app/api/user/user.py
@@ -5,6 +5,12 @@ from rest_framework.response import Response
 class User(APIView):
     
     def post(self, request):
+        
+        input_email = request.POST['email']
+        input_pw = request.POST['password']
+        
+        print(f'이메일 - {input_email}, 비번 - {input_pw}')
+        
         return Response({
             'code': 200,
             'message': '임시- 로그인 기능'
--- copang_project/urls.py
+++ copang_project/urls.py
@@ -20,6 +20,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 
+from copang_app.api.user import User
+
 # swagger 정보 사항 세팅 -> schema_view 변수에 담아두기
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,8 +38,11 @@ schema_view = get_schema_view(
 
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user', User.as_view(), name='user'),
+    
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
     re_path(r'^api/docs$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
