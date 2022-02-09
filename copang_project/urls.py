5eaeeaedab1554237db5426b38c956b8c983cf62
이승훈 <negrat@naver.com>
2022-02-09 10:16:52 +0900

    swagger 주소만 열어주기
    - 정보 사항 편집 필요

--- copang_project/urls.py
+++ copang_project/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+# swagger 정보 사항 세팅 -> schema_view 변수에 담아두기
+schema_view = get_schema_view(
+    openapi.Info(
+        title='사이트 제목',
+        default_version='v1',
+        description='사이트 설명 문구',
+        terms_of_service="이용약관링크",
+        contact=openapi.Contact(name='Tester', email='test@test.com'),
+        license=openapi.License(name='실제적용라이센스')
+    ),
+    public=True,
+    permission_classes=(AllowAny,)
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    re_path(r'^api/docs$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
