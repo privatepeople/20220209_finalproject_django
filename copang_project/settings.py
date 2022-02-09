1c28824a45b399914f726a7fd6f0809fb1f4606a
이승훈 <negrat@naver.com>
2022-02-09 09:47:37 +0900

    db 접속 정보 수정

--- copang_project/settings.py
+++ copang_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'copang_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'my-first-db2.ckcb9pt3t4a9.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'NAME': 'final_project',
     }
 }
 
