437da5a23f4a0e5aba431a47c10669257ac0e17e
이승훈 <negrat@naver.com>
2022-02-09 10:05:10 +0900

    Users모델 > dict 변환 클래스 추가

---
+++ copang_app/serializers.py
@@ -0,0 +1,9 @@
+# queryset > dict로 자동 변환 기능.
+
+from rest_framework import serializers
+from .models import Users
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Users
+        fields = '__all__'
